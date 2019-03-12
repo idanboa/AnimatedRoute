ce14fc0b811c8e4fae84cde9fbd2a77b7f17ed72
Idan Boadana <idan.boadana@gett.com>
2019-03-12 16:51:47 +0200

    initial commit

---
+++ AnimatesRoute/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatesRoute
+//
+//  Created by Idan Boadana on 3/12/19.
+//  Copyright © 2019 Idan Boadana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
